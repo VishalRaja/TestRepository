7ab21df0bd35ac3a5dfa51fb518e9ee211f0c6e7
Training_C2a.05.01 <Training_C2a.05.01@CDC2-D-CFWPXJ2.dir.svc.accenture.com>
2019-07-19 12:00:50 +0530

    Commit message for test repository

---
+++ src/test/java/testPack/TestRunner.java
@@ -0,0 +1,12 @@
+package testPack;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="C:\\Users\\training_c2a.05.01\\Desktop\\pom\\POM-Cucumber\\src\\test\\resource\\Login.feature"
+		)
+public class TestRunner {
+}
